b3fdca7405a381fe0213325ff1f8e61829247346
GeorgeOrewell <georgeorewell@mein.gmx>
2024-09-25 23:25:09 +0200

    "test3"

--- chatbot.js
+++ chatbot.js
@@ -13,7 +13,7 @@ const fuse = new Fuse(fuseData, {
 
 // Funktion, die den Bot dazu bringt, eine Antwort basierend auf der Nutzereingabe zu finden
 function getBotReply(message) {
-  let cleanput = cleanString(message);
+//  let cleanput = cleanString(message);
   const result = fuse.search(cleanput); // Fuse.js verwendet die Suche
 
   if (result.length > 0) {
